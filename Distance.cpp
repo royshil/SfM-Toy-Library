9fdb77dee94065de4b4d5eb3fd0f5f1971a422d9
Roy Shilkrot <roy.shil@gmail.com>
2012-05-15 08:12:27 -0400

    fltk not found - use command line gui

--- Distance.cpp
+++ Distance.cpp
@@ -7,6 +7,8 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/video/tracking.hpp"
 #include <fstream>
+#include "dirent.h"
+
 
 using namespace std;
 
@@ -15,7 +17,8 @@ using namespace cv;
 
 #include "Distance.h"
 
-
+#ifndef NO_FLTK
+//------------------------------ Using FLTK GUI ------------------------------
 int runUI(int argc, char** argv);
 
 int main(int argc, char** argv) { //test with real photos
@@ -28,3 +31,69 @@ int main(int argc, char** argv) { //test with real photos
     return 0;
 
 }
+
+#else
+//---------------------------- Using command-line ----------------------------
+
+#include "MultiCameraPnP.h"
+
+
+
+std::vector<cv::Mat> images;
+std::vector<std::string> images_names;
+
+bool hasEnding (std::string const &fullString, std::string const &ending)
+{
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+void open_imgs_dir(char* dir_name) {
+	if (dir_name == NULL) {
+		return;
+	}
+	std::vector<std::string> files_;
+	DIR *dp;
+	struct dirent *ep;     
+	dp = opendir (dir_name);
+	
+	if (dp != NULL)
+	{
+		while (ep = readdir (dp)) {
+			if (ep->d_name[0] != '.')
+				files_.push_back(ep->d_name);
+		}
+		
+		(void) closedir (dp);
+	}
+	else {
+		std::cerr << ("Couldn't open the directory");
+		return;
+	}
+	for (unsigned int i=0; i<files_.size(); i++) {
+		if (files_[i][0] == '.' || !(hasEnding(files_[i],"jpg")||hasEnding(files_[i],"png"))) {
+			continue;
+		}
+		cv::Mat m_ = cv::imread(std::string(dir_name) + "/" + files_[i]);
+		images_names.push_back(files_[i]);
+		images.push_back(m_);
+	}
+}
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		cerr << "USAGE: " << argv[0] << " <path_to_images>" << endl;
+		return 0;
+	}
+	
+	open_imgs_dir(argv[1]);
+	
+	cv::Ptr<IDistance> distance = new MultiCameraPnP(images,images_names);
+	distance->RecoverDepthFromImages();
+	
+	RunVisualization(distance->getPointCloud(), distance->getPointCloudRGB());
+}
+#endif
--- DrawKeypoints.cpp
+++ DrawKeypoints.cpp
@@ -6,6 +6,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "FeatureMatching.h"
 #include <libgen.h>
+#include "FindCameraMatrices.h"
 
 using namespace cv;
 using namespace std;
@@ -44,36 +45,37 @@ int main( int argc, char** argv )
 		extractor.compute(img_2, keypoints_2, descriptors_2);
 		
 		BruteForceMatcher<L2<float> > matcher;
-		vector<DMatch> matches;
-		matcher.match(descriptors_1, descriptors_2, matches);
-		
-		vector<Point2f> pts1,pts2;
-		for (unsigned int i=0; i<matches.size(); i++) {
-			pts1.push_back(keypoints_1[matches[i].queryIdx].pt);
-			pts2.push_back(keypoints_2[matches[i].trainIdx].pt);
-		}
-		vector<uchar> status;
-		Mat F = findFundamentalMat(pts1, pts2, FM_RANSAC, 0.5, 0.99, status);
-
-		cout << "F " << F << endl;
-
-		vector<KeyPoint> kpts1,kpts2;
 		vector<DMatch> Fmatches;
-		for (unsigned int i=0; i<pts1.size(); i++) {
-			if (status[i]) {
-				cout << "Fmatch " << i << endl;
-				Fmatches.push_back(DMatch(kpts1.size(),kpts1.size(),1));
-				kpts1.push_back(KeyPoint(pts1[i],1));
-				kpts2.push_back(KeyPoint(pts2[i],1));
-			}
-		}
+		matcher.match(descriptors_1, descriptors_2, Fmatches);
 		
-		drawMatches(img_1, kpts1, img_2, kpts2, Fmatches, img_keypoints, Scalar::all(-1), Scalar::all(-1));
+		//vector<Point2f> pts1,pts2;
+//		for (unsigned int i=0; i<matches.size(); i++) {
+//			pts1.push_back(keypoints_1[matches[i].queryIdx].pt);
+//			pts2.push_back(keypoints_2[matches[i].trainIdx].pt);
+//		}
+//		vector<uchar> status;
+//		Mat F = findFundamentalMat(pts1, pts2, FM_RANSAC, 0.5, 0.99, status);
+//
+//		cout << "F " << F << endl;
+//
+		vector<KeyPoint> kpts1,kpts2;
+//		vector<DMatch> Fmatches;
+//		for (unsigned int i=0; i<pts1.size(); i++) {
+//			if (status[i]) {
+//				cout << "Fmatch " << i << endl;
+//				Fmatches.push_back(DMatch(kpts1.size(),kpts1.size(),1));
+//				kpts1.push_back(KeyPoint(pts1[i],1));
+//				kpts2.push_back(KeyPoint(pts2[i],1));
+//			}
+//		}
+		GetFundamentalMat(keypoints_1, keypoints_2, kpts1, kpts2, Fmatches);
+		
+		drawMatches(img_1, keypoints_1, img_2, keypoints_2, Fmatches, img_keypoints, Scalar::all(-1), Scalar::all(-1), Mat(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 	}
 
 	
 	
-	imwrite(std::string(basename(argv[1])) + "_keypoints.jpg", img_keypoints);
+	imwrite(std::string(basename(argv[1])) + "_keypoints.png", img_keypoints);
 	
 	return 0;
 }
